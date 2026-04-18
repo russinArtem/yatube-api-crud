85a611ba0610e59aff42071d42de8c5d5ab4cf69
Artem Russin <russinartem@yandex.ru>
2026-04-18 13:09:50 +0300

    v.3.0

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,16 +7,14 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 v1_router = DefaultRouter()
 v1_router.register(r'posts', PostViewSet, basename='posts')
 v1_router.register(r'groups', GroupViewSet, basename='groups')
-
-v1_comments_router = DefaultRouter()
-v1_comments_router.register(r'comments', CommentViewSet, basename='comments')
+v1_router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
 ] + v1_router.urls
-
-urlpatterns += [
-    path('posts/<int:post_id>/', include(v1_comments_router.urls)),
-]
 
 urlpatterns = [path('v1/', include(urlpatterns))]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,9 +7,10 @@ from posts.models import Group, Post
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, instance):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return instance.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or instance.author == request.user
+        )
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -28,16 +29,13 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    lookup_field = 'pk'
-    lookup_url_kwarg = 'comment_id'
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
-    def initial(self, request, *args, **kwargs):
-        super().initial(request, *args, **kwargs)
-        self._post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
 
     def get_queryset(self):
-        return self._post.comments.all()
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        serializer.save(post=self._post, author=self.request.user)
+        serializer.save(post=self.get_post(), author=self.request.user)
